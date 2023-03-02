2db3cc7f13d364fb2144405e261eac19505c1205
Ariya Adinatha <ariyaadinatha21@gmail.com>
2023-03-03 01:35:37 +0700

    feat: added value detection for secret

--- src/testcase/python/Basic_approaches.py
+++ src/testcase/python/Basic_approaches.py
@@ -4,10 +4,17 @@ var2 = 123
 def halo(param):
 	return param+1
 
+password1 = "aBc123!@#"
+password2 = "12345678"
+key = "secretkey"
+api_key = "api_key_here"
+bruh = "waiwIASdij123"
+
+
 password = "halo"
 
 if nama == "budi":
-    print("Hai budi")
+    print("Hai budi", 123, "https://tree-sitter.github.io/tree-sitter/playground", "iniAdalahP4ssw0rd!", "ini4n4k4lY")
     varqweqweqweqwe = 1
-    halo(var)
+    halo(var, 123, "bruh")
     sleep(1)
