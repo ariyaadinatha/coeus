f04d3b4db3cfd5a72b119f30908a578a47edb8a4
fakhrinail <fakhrinailwbw@gmail.com>
2023-06-05 17:27:27 +0700

    test: add new test for python dfg

---
+++ src/testcase/python/if_else.py
@@ -0,0 +1,18 @@
+test = "test"
+
+if test == "test":
+    test = "uhuy"
+elif test == "test":
+    test = "ahay"
+else:
+    test = "asoy"
+
+if test == "test":
+    print(test)
+    test = "uhuy"
+elif test == "test":
+    print(test)
+    test = "ahay"
+else:
+    print(test)
+    test = "asoy"
---
+++ src/testcase/python/multiple_level_if_else.py
@@ -0,0 +1,7 @@
+test = "test"
+
+if test == "level 1":
+    if test == "level 2":
+        sql_query = "SELECT * FROM introduction_login WHERE user='"+test+"test"+test
+        if test == "level 3":
+            val = model.objects.raw(sql_query)
