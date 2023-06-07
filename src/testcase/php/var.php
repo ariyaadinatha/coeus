f66da8a5e4f572d90ae743d80c88709cc0affad5
fakhrinail <fakhrinailwbw@gmail.com>
2023-06-07 20:52:07 +0700

    test: add testcase for php

---
+++ src/testcase/php/var.php
@@ -0,0 +1,4 @@
+<?php
+$query  = "SELECT first_name, last_name FROM users WHERE user_id = '$id';";
+$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
+?>
