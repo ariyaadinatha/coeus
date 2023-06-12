1ac05c466be3cd01338f5b2bbcc3f4da4f5441e0
fakhrinail <fakhrinailwbw@gmail.com>
2023-06-12 17:44:30 +0700

    feat(java): add java identifiers

--- src/utils/constant/intermediate_representation.py
+++ src/utils/constant/intermediate_representation.py
@@ -6,4 +6,5 @@ JAVASCRIPT_CONTROL_SCOPE_IDENTIFIERS = ("statement_block", "elif_clause", "else_
 PHP_CONTROL_SCOPE_IDENTIFIERS = ("if_statement", "else_if_clause", "else_clause")
 PHP_DATA_SCOPE_IDENTIFIERS = ("function_definition", "class_declaration")
 
-JAVA_CONTROL_SCOPE_IDENTIFIERS = ("statement_block", "elif_clause", "else_clause")
+JAVA_DATA_SCOPE_IDENTIFIERS = ("class_declaration", "method_declaration")
+JAVA_CONTROL_SCOPE_IDENTIFIERS = ("block", "elif_clause", "else_clause")
