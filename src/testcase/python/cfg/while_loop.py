9c0792fdb42b8c68787ae9a26bcec5dfa23aa21c
fakhrinail <fakhrinailwbw@gmail.com>
2023-06-12 20:27:04 +0700

    test(python): add cfg testcases

---
+++ src/testcase/python/cfg/while_loop.py
@@ -0,0 +1,3 @@
+fruits = ["apple", "banana", "cherry"]
+while True:
+    print(fruits)
--- src/utils/constant/intermediate_representation.py
+++ src/utils/constant/intermediate_representation.py
@@ -1,6 +1,6 @@
 PYTHON_CONTROL_SCOPE_IDENTIFIERS = ("block", "elif_clause", "else_clause")
 PYTHON_CONTROL_STATEMENTS = ("if_statement", "for_statement", "while_statement", "try_statement")
-PYTHON_DIVERGE_CONTROL_STATEMENTS = ("elif_clause", "else_clause", "except_clause")
+PYTHON_DIVERGE_CONTROL_STATEMENTS = ("elif_clause", "else_clause", "except_clause", "finally_clause")
 PYTHON_DATA_SCOPE_IDENTIFIERS = ("class_definition", "function_definition")
 
 JAVASCRIPT_CONTROL_SCOPE_IDENTIFIERS = ("statement_block", "elif_clause", "else_clause")
