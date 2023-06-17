2eabe1c83c140b0eacf4df42e844f7c6d5a7009f
fakhrinail <fakhrinailwbw@gmail.com>
2023-06-17 11:30:04 +0700

    test(js): add more testcases

--- src/testcase/javascript/cfg/if_else_if.js
+++ src/testcase/javascript/cfg/if_else_if.js
@@ -1,9 +1,9 @@
 if (time < 10) {
   greeting = "Good morning";
 } else {
-if (time < 20) {
-    greeting = "Good day";
-  } else {
-    greeting = "Good evening";
-  }
+  if (time < 20) {
+      greeting = "Good day";
+    } else {
+      greeting = "Good evening";
+    }
 }
