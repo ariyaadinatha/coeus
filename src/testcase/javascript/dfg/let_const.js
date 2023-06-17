e0997fa05543f3d057c70728ebeaf9663e36bb13
fakhrinail <fakhrinailwbw@gmail.com>
2023-06-17 16:39:05 +0700

    test(js): add testcases for block scope problems

--- src/testcase/javascript/dfg/let_const.js
+++ src/testcase/javascript/dfg/let_const.js
@@ -1,3 +1,14 @@
-const price1 = 5;
-const price2 = 6;
-let total = price1 + price2;
+const price = 1;
+
+if (price == 1) {
+  let price2 = 2;
+  if (price == 1) {
+    price2 = 'ehhehee';
+    console.log(price2);
+  }
+} else {
+  let price3 = 3;
+  if (price == 3) {
+    console.log(price3);
+  }
+}
