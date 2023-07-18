534c30a1d99c6773f56e23f5e41aa0d6f7756b24
fakhrinail <fakhrinailwbw@gmail.com>
2023-07-18 21:44:12 +0700

    feat: add swap dfg testcases

---
+++ src/testcase/graph/dfg/swap.java
@@ -0,0 +1,9 @@
+int x = 10;
+int y = 20;
+
+int temp = x;
+x = y;
+y = temp;
+
+System.out.println(x);
+System.out.println(y);
---
+++ src/testcase/graph/dfg/swap.js
@@ -0,0 +1,9 @@
+a = "value"
+b = "other"
+
+temp = a
+a = b
+b = temp
+
+console.log(a)
+console.log(b)
